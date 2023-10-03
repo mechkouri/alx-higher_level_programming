7cd6ba1414b1ef0eccfab6f12bb520bb5f9304f0
mechkouri <saidmechkouri@gmail.com>
2023-10-03 14:59:59 -0700

    documment updated

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - mechkouri said
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author - said mechkouri
+
+def print_last_digit(number):
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
